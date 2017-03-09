75fd63ff5a296bb28f87143cadb46774d24329b1
Camila Kutscher <cakutscher@uc.cl>
2017-03-09 14:46:38 -0300

    tables

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :comments
   resources :feeds
-  resources :noticia
-  resources :news
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #root "welcome#index"
+  root "feeds#index"
+
 end
